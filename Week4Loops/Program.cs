659cec9a82acf87471d828521777a5bcc92abdf8
Anett K <anett.kollin@tktk.ee>
2023-05-13 12:56:07 +0300

    add week4loops

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿// FOR-LOOP
+// DRY-principle - Do Not Repeat Yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++) // i++ = i + 1
+{
+    Console.WriteLine("we don't need no education");
+}
+
+Console.WriteLine(i);
